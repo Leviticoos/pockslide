33defceabaac1e2d221b65143677403d4970f03e
Leviticoos <105832125+Leviticoos@users.noreply.github.com>
2023-10-13 07:37:09 -0700

    Update pocksward.py

--- pocksward.py
+++ pocksward.py
@@ -6,12 +6,60 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from enum import Enum
+from random import random
 
 
 #Game Code
 
-class Game:
+class Check:
+    def __init__(self, skillTransform,n=1, helpText=''):
+        '''
+        Define a check, which is a base unit of a play. Takes in a skill transform, which will map an action vector to different skills, and possibly 
+        n is an intager that represents how many chances you have to succseed. This is a way of letting low chances be more likely. 
+        helpText, which can be your notes on what this check is.
+        
+        Conceptually, a check is a single pass/fail unit of the sport. The player will set their action vector, and 
+        '''
+        self.playTrans = np.array(skillTransform)
+        self.n = int(n)
+        self.help = helpText
 
+    def help(self):
+        return self.help
+    
+    def run(self, playVtr, actVtr, sklVtr, verbose=False):
+        '''
+        playVtr is a 1D vector that defines the current play vector. If it's a thrown ball, think of it as how it was thrown. It will be modified per the check's play Transform Vector. Must be n long.
+        actVtr is a 1D vector, same length as above, that defines the action the player is making
+        sklVtr is a 1D vector, length n, that defines how good a player is at different tasks. 
+        
+        If verbose is false, returns bool value of if check was sucsess (True) or a failure (False)
+        If verbose is True, good luck Charlie
+        '''
+        adjPlayVtr = np.array(playVtr).dot(self.playTrans)
+        adjActVtr = np.multiply(np.array(actVtr), np.array(sklVtr))
+        prob = adjPlayVtr.dot(np.array(adjActVtr))
+        probN = 1-(1-prob)**self.n
+        roll = random()
+        
+        if roll<probN:
+            check = True
+        else:
+            check = False
+        
+        if verbose:
+            return check, probN, roll, adjActVtr
+        else:
+            return check
+
+
+class Game:
+    def __init__(self, t):
+        self.t = t
+        
+    def run():
+        self.t = 0
 
 #Peaple Code
 
@@ -44,5 +92,15 @@ class Coach:
 
 class Team:
     '''Team is a class that contains a coach, a list of players (5), stats, and a N.N. to control personel decisions'''
-
+    def __init__(self, coach, players, stats=np.zeros(10)):
+        self.coach = coach
+        self.players = players
+        self.stats = stats
+        
+        
+    def fireCoach(self, newCoach):
+        self.coach = newCoach
+        
+    def updateStats(self, newStats):
+        self.stats.append(newStats)
 
